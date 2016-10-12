b3aa5bd6b6d9fff37964b688e2abdfa3c7c7ae3d
Charkey <charkeyqk@gmail.com>
2016-10-12 11:12:03 +0800

    dynamic data

--- src/components/SecondCmpt.vue
+++ src/components/SecondCmpt.vue
@@ -19,6 +19,24 @@
         author: "微信公众号",
         articles: []
       }
+    },
+    mounted: function() {
+      // 钩子函数, see also 官方文档关于 vue 生命周期的解析 http://vuejs.org/guide/instance.html#Lifecycle-Diagram
+      this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+        headers: {
+
+        },
+        emulateJSON: true
+      }).then(function(response) {
+        // 这里是处理正确的回调
+
+        this.articles = response.data.subjects
+        // this.articles = response.data["subjects"] 也可以
+
+      }, function(response) {
+        // 这里是处理错误的回调
+        console.log(response)
+      });
     }
   }
 </script>
