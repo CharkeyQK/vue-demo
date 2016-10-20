b2f309895085d1b1df1755c6085f5819b5a5400d
Charkey <charkeyqk@gmail.com>
2016-10-20 14:55:04 +0800

    ListOrderDemo

--- src/components/EChartsDemo.vue
+++ src/components/EChartsDemo.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="echartsBarDemo" style="width: 500px;height: 300px;"></div>
+  <div>
+    <div id="echartsBarDemo" style="width: 500px;height: 300px;"></div>
+    <div id="echartsLineDemo" style="width: 500px;height: 300px;"></div>
+  </div>
 </template>
 
 <script>
@@ -24,6 +27,39 @@
       };
       // 绘制图表
       myBarChart.setOption(barOption);
+
+      var myLineChart = echarts.init(document.getElementById('echartsLineDemo'));
+      var lineOption = {
+        title: {
+          text: '动态数据 + 时间坐标轴'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            animation: false
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          name: "月份"
+        },
+        yAxis: [
+          {
+            type: 'value',
+            splitArea: { show: true },
+            name:"数值"
+          }
+        ],
+        series: [{
+          name: '模拟数据',
+          type: 'line',
+          showSymbol: false,
+          hoverAnimation: false,
+          data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+        }]
+      };
+      myLineChart.setOption(lineOption);
     }
   }
 
