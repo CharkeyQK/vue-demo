2c2bd5b3783268898da79c7f70c4a57864704158
Charkey <charkeyqk@gmail.com>
2016-10-25 15:24:30 +0800

    component

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
     </ul>
     <router-view class="view"></router-view>
     <div style="text-align: left;">
-      <ListOrderDemo></ListOrderDemo>
+      <ListOrderDemo v-bind:data="orderData"></ListOrderDemo>
       <EChartsDemo></EChartsDemo>
     </div>
   </div>
@@ -36,7 +36,40 @@ import EChartsDemo from './components/EChartsDemo'
 export default {
   components: {
     Hello, FirstCmpt, SecondCmpt,ListOrderDemo,EChartsDemo
-  }
+  },
+  data() {
+    return {
+      orderData: {
+        "ordRepo": "EBOSS通用仓库",
+        "ordStatus": "待发货",
+        "orderItems": [
+          {
+            "baoZhang": "icon111",
+            "count": "111",
+            "itemImg": "http://10.19.18.18:6465/6d0a07c3709142719b7644e4a5c48fcc.jpg",
+            "itemName": "情意中秋~华美中秋豪华月饼礼盒111",
+            "price": "123.00"
+          },
+          {
+            "baoZhang": "icon222",
+            "count": "222",
+            "itemImg": "http://10.19.18.18:6465/6d0a07c3709142719b7644e4a5c48fcc.jpg",
+            "itemName": "情意中秋~华美中秋豪华月饼礼盒222",
+            "price": "456.00"
+          },
+          {
+            "baoZhang": "icon333",
+            "count": "333",
+            "itemImg": "http://10.19.18.18:6465/6d0a07c3709142719b7644e4a5c48fcc.jpg",
+            "itemName": "情意中秋~华美中秋豪华月饼礼盒333",
+            "price": "789.00"
+          }
+        ],
+        "receiver": "Charkey",
+        "total": "299.00"
+      }
+    };
+  },
 }
 </script>
 
